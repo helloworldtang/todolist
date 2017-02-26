a154d559d56d382114fb9bd13ca83a066b96ac24
Tangcheng <helloworld.tang@qq.com>
2017-02-26 22:32:07 +0800

    fix bug process white label ssl support and auto redirect to ssl port 8443

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # todolist
-demo
+items:  
+visit http://localhost/MissingPage.png
+redirect to https://localhost:8443/MissingPage.png
+  
+todo:  
+Resource interpreted as Stylesheet but transferred with MIME type text/html: "https://localhost:8443/style.css".
---
+++ src/main/java/com/lpp/config/SSLConfig.java
@@ -0,0 +1,43 @@
+package com.lpp.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by tangcheng on 2017/2/26.
+ */
+@Configuration
+public class SSLConfig {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainerFactory() {
+        TomcatEmbeddedServletContainerFactory containerFactory = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        containerFactory.addAdditionalTomcatConnectors(httpConnector());
+        return containerFactory;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+}
