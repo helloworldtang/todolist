6f4b0b20135aaa1582d24a5e355ab58d44757494
Tangcheng <helloworld.tang@qq.com>
2017-06-14 00:29:41 +0800

    禁用SSL中的主机名检查，只在dev环境中使用

--- src/main/java/com/lpp/config/SSLConfig.java
+++ src/main/java/com/lpp/config/SSLConfig.java
@@ -4,16 +4,29 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by tangcheng on 2017/2/26.
  */
 @Configuration
 public class SSLConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SSLConfig.class);
+
+    static {
+        LOGGER.info("下面这行代码将禁用SSL中的主机名检查。只在开发期间使用");
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, sslSession) -> true);
+    }
+
+
     @Bean
     public EmbeddedServletContainerFactory servletContainerFactory() {
         TomcatEmbeddedServletContainerFactory containerFactory = new TomcatEmbeddedServletContainerFactory() {
